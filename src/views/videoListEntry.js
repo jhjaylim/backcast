3838c79110e017377e51cce1e1bb30ffc2aff8e7
Jay Lim <jhjaylim@gmail.com>
2017-07-18 11:37:35 -0700

    Finish list view with minor revision

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,9 +1,13 @@
 var VideoListEntryView = Backbone.View.extend({
 
+
+
   render: function() {
   	this.$el.html(this.template(this.model.attributes));
   	this.$el.find('.media-body .video-list-entry-title').html(this.model.attributes.snippet.title);
   	this.$el.find('.media-body .video-list-entry-detail').html(this.model.attributes.snippet.description);
+
+  	this.$el.find('.media-left .media-object').attr('src', this.model.attributes.snippet.thumbnails.default.url);
 
     return this;
   },
